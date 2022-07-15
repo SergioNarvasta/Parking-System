7a0cde598b3ddf2829da0bd7fb0d0fd2290c87ed
Sergio Narvasta <sergio.jn024@gmail.com>
2022-07-15 00:50:46 -0500

    Update Project Architecture

---
+++ Pharmacy/PharmacyBA/Models/TipoUsuario.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace PharmacyBA.Models
+
+{
+    public class TipoUsuario
+    {
+        [Key]
+        public int Tiu_codepk { get; set; }
+
+        [MaxLength (45)]
+        public String ?Tiu_desc { get; set; }
+    }
+}
---
+++ Pharmacy/PharmacyBA/Models/Usuario.cs
@@ -0,0 +1,7 @@
+﻿namespace PharmacyBA.Models
+{
+    public class Usuario
+    {
+
+    }
+}
