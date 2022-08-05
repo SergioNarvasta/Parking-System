5341ec944242d6528c26de113c0b51a6e86430d8
Sergio Narvasta <sergio.jn024@gmail.com>
2022-08-04 23:29:57 -0500

    Enable Cors

---
+++ VET-Backend/EjemploSIST/Controllers/EnableCors.cs
@@ -0,0 +1,44 @@
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins"; //Add
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>                     //Add
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("http://example.com",
+                                              "http://www.contoso.com")
+                                               .WithMethods("PUT", "DELETE", "GET");
+                      });
+});
+
+builder.Services.AddControllers();
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+
+app.UseCors();      //Add
+
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>    //Add
+{
+    endpoints.MapGet("/echo",
+        context => context.Response.WriteAsync("echo"))
+        .RequireCors(MyAllowSpecificOrigins);
+
+    endpoints.MapControllers()
+             .RequireCors(MyAllowSpecificOrigins);
+
+    endpoints.MapGet("/echo2",
+        context => context.Response.WriteAsync("echo2"));
+
+    endpoints.MapRazorPages();
+});
+
+app.Run();
