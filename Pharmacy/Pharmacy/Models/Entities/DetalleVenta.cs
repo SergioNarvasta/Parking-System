3fe6f8cbaf429ca10b498ba9715d0636584616c5
Innovation Services Peru <sergio.jn024@gmail.com>
2023-01-16 20:42:12 -0500

    Dev Detail

--- Pharmacy/Pharmacy/Models/Entities/DetalleVenta.cs
+++ Pharmacy/Pharmacy/Models/Entities/DetalleVenta.cs
@@ -6,11 +6,16 @@ namespace Pharmacy.Models.Entities
     public class DetalleVenta
     {
         [Key]
-        public int CodDetVenta { get; set; }
+        public int IdDetVenta   { get; set; }
+        public int IdProducto   { get; set; }
         public int Cantidad     { get; set; }
-        public Double Precio    { get; set; }
+        //public Double Precio  { get; set; }
         public Double Subtotal  { get; set; }
         public Double IGV       { get; set; }
+        
+        public int RegCliente   { get; set; } //Flag para sabser ri se registro el cliente(1) o no(0)
+        public int IdCliente    { get; set; }
+        public string NomCliente{ get; set; }
 
         //ForeignKey Key
         public int CodVenta { get; set; }
